4db4bbf0a6100c7c0c4a6458705d73a1ba774495
Camila-rodriguezz <a.22308051281300@cbtis128.edu.mx>
2024-11-25 17:26:17 -0600

    Add files via upload

---
+++ UIII_P1_Tabla_Materia_CRUD_v5.1/empleado_app/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render,redirect 
+from .models import Empleados
+# Create your views here.
+
+def inicio_vistaEmpleados(request):
+    losempleados=Empleados.objects.all()
+    return render(request,"gestionarEmpleados.html",{"misempleados":losempleados})
+
+def registrarEmpleado(request):
+    id_empleado=request.POST["txtempleado"]
+    nombre=request.POST["txtnombre"]
+    email=request.POST["txtemail"]
+    curp=request.POST["txtcurp"]
+    acta_de_nacimiento=request.POST["txtnacimiento"]
+    numero_celular=request.POST["txtnum"]
+    rfc=request.POST["txtrfc"]
+    id_sucursales=request.POST["txtsucursales"]
+    direccion=request.POST["txtdireccion"]
+
+    guardarempleados=Empleados.objects.create(
+        id_empleado=id_empleado,nombre=nombre,email=email,curp=curp,acta_de_nacimiento=acta_de_nacimiento,numero_celular=numero_celular,rfc=rfc,id_sucursales=id_sucursales,direccion=direccion
+    ) # GUARDA EL REGISTRO
+
+    return redirect("empleado")
+
+def seleccionarEmpleado(request,id_empleado):
+    empleado=Empleados.objects.get(id_empleado=id_empleado)
+    return render(request,"editarempleado.html",{"misempleados":empleado})
+
+def editarEmpleado(request):
+    id_empleado=request.POST["txtempleado"]
+    nombre=request.POST["txtnombre"]
+    email=request.POST["txtemail"]
+    curp=request.POST["txtcurp"]
+    acta_de_nacimiento=request.POST["txtnacimiento"]
+    numero_celular=request.POST["txtnum"]
+    rfc=request.POST["txtrfc"]
+    id_sucursales=request.POST["txtsucursales"]
+    direccion=request.POST["txtdireccion"]
+    empleado=Empleados.objects.get(id_empleado=id_empleado)
+    empleado.nombre=nombre
+    empleado.email=email
+    empleado.curp=curp
+    empleado.acta_de_nacimiento=acta_de_nacimiento
+    empleado.numero_celular=numero_celular
+    empleado.rfc=rfc
+    empleado.id_sucursales=id_sucursales
+    empleado.direccion=direccion
+    empleado.save() # GUARDA EL REGISTRO
+    return redirect("empleado")
+
+def borrarEmpleado (request,id_empleado):
+    empleado=Empleados.objects.get(id_empleado=id_empleado)
+    empleado.delete() # ELIMINA EL REGISTRO
+    return redirect("empleado")
